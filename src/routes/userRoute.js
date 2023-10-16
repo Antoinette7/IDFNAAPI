9741a190206139c1a10690b12d03f37f29194de3
Antoinette7 <antoinettenyiransengimana@gmail.com>
2023-10-16 05:13:00 +0200

    users

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -11,7 +11,7 @@ import {
 const userRoutes = express.Router();
 userRoutes.delete("/delete/:id", deletuser);
 userRoutes.get("/read",getalluser);
-userRoutes.post("/usersignup", fileUpload.single("profile"),signup);
+userRoutes.post("/signup", fileUpload.single("profile"),signup);
 userRoutes.post("/login", fileUpload.single("profile"),login);
 userRoutes.put("/update/:id" ,fileUpload.single("profile"), updateData);
 export default userRoutes;
