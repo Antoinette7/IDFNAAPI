606802f1a2ba230d810319df4c1786971b7a99d4
Antoinette7 <antoinettenyiransengimana@gmail.com>
2023-10-27 14:46:19 +0200

    autho

--- src/routes/commentRoutes.js
+++ src/routes/commentRoutes.js
@@ -10,11 +10,10 @@ import fileUpload from "../helper/multer";
 
 const commentRoutes = express.Router();
 
-commentRoutes.post("/create/:blogId", fileUpload.single("blogImage"), createComment);
+commentRoutes.post("/create/:blogId", Authorization, fileUpload.single("blogImage"), createComment);
 
 commentRoutes.get('/read', getComments );
 
 commentRoutes.delete("/delete/:id", commentdelete);
-
 
 export default commentRoutes 
